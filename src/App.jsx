642eff917e15017f4be056eaa7ca52cda67d55fc
Andrii Valenko <andreyvalenko@gmail.com>
2023-05-25 14:06:15 +0300

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
-import Router from "./components/Router";
-import ThemeProvider from "@mui/material/styles/ThemeProvider";
-import { theme } from "./theme/theme";
-
 const App = () => {
   return (
-    <ThemeProvider theme={theme}>
-      <Router />
-    </ThemeProvider>
+    <>
+      <p>Hello world!</p>
+      <p>Hello world!</p>
+    </>
   );
 };
 
