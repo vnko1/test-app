1720b57778f2dc93dd717c5904f36aacb740f714
Andrii Valenko <andreyvalenko@gmail.com>
2023-05-25 14:47:49 +0300

    test

--- src/index.js
+++ src/index.js
@@ -1,23 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/integration/react";
-import { BrowserRouter } from "react-router-dom";
-import { store, persistStor } from "./redux/index";
+
 import App from "./App.jsx";
-import UsersProvider from "./components/UsersContext";
-import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistStor}>
-        <UsersProvider>
-          <BrowserRouter basename="/test-app">
-            <App />
-          </BrowserRouter>
-        </UsersProvider>
-      </PersistGate>
-    </Provider>
+    <App />
   </React.StrictMode>
 );
